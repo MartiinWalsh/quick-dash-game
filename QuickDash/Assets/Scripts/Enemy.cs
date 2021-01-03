38e507acfd117346b3143c4f7a8a41a1e6e1ddd4
m-codes <martinwalsh.dev@gmail.com>
2021-01-03 19:49:17 +0000

    Sprites, animations, player & enemy script added

---
+++ QuickDash/Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float minSpeed;
+    public float maxSpeed;
+
+    float speed;
+
+    Player playerScript;
+    public int damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(minSpeed, maxSpeed);
+        playerScript = FindObjectOfType<Player>().GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector2.down == x being 0 and y being -1
+        //delta time used for performance across computers
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            playerScript.Takedamage(damage);
+        }
+    }
+}
---
+++ QuickDash/Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private float input;
+
+    
+    Rigidbody2D rb;
+    Animator anim;
+
+    public int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        
+        if (input != 0)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+
+        Vector3 localScale = transform.localScale;
+        // Moving right
+        if (input > 0)
+        {
+            localScale.x = 1;
+        }
+        else if (input < 0)
+        {
+            localScale.x = -1;
+        }
+        transform.localScale = localScale;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        input = Input.GetAxisRaw("Horizontal");
+
+
+        // Moving the player
+        rb.velocity = new Vector2(input * speed, rb.velocity.y);
+    }
+
+    public void Takedamage(int damageAmount)
+    {
+        health -= damageAmount;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
